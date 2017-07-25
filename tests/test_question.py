8c69a11081bde2f7eacb4ea4f94ef2776812d447
songhm <songhm@qssec.com>
2017-07-25 21:55:47 +0800

    fix env

--- tests/test_question.py
+++ tests/test_question.py
@@ -11,9 +11,9 @@ class QuestionTest(BaseTestCase):
             "question": "hhh"
         }
         name_list = Question.objects.values_list('question_text')
-        self.assertNotIn(data['question_text'], name_list)
+        self.assertNotIn(data['question'], name_list)
         response = self.client.post(self.url, json.dumps(data), content_type='application/json')
         self.assertEqual(response.status_code, 200)
         new_names = Question.objects.values('question_text')
         name_lists = [name['question_text'] for name in new_names]
-        self.assertNotIn(data['question_text'], name_lists)
+        self.assertNotIn(data['question'], name_lists)
